bce7262c98fae82a8933f0803a991b0895ec346a
Oumrath <101456082+Oumrath@users.noreply.github.com>
2022-03-12 17:03:12 +0545

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(100, 100, 20, 20);
+}
+
+function draw() 
+{
+  background("brown");
+  if (keyIsDown(RIGHT_ARROW))
+  {
+  box.position.x = box.position.x + 2;
+
+  }
+  if (keyIsDown(LEFT_ARROW))
+  {
+  box.position.x = box.position.x - 2;
+
+  }
+  if (keyIsDown(UP_ARROW))
+  {
+  box.position.y = box.position.y - 2;
+
+  }
+  if (keyIsDown(DOWN_ARROW))
+  {
+  box.position.y = box.position.y + 2;
+
+  }
+
+
+  drawSprites();
+  
+
+}
+
+
+
+
